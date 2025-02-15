cb074051e8790916a43188c618e0f87131120cd3
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2025-02-16 02:22:03 +0530

    Add files via upload

---
+++ basic for.py
@@ -0,0 +1,10 @@
+name="Kunal"
+print("Hello,"+name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print("Lets Use for Loop\n")
+for character in name:
+    print(character)
